ee16928fcd0fd6c0cec485aba1e07c5d84688367
rahulgupta020 <rahul.gupta@factsonline.in>
2024-06-27 16:43:40 +0530

    adding code for multiplication function

--- main.py
+++ main.py
@@ -3,11 +3,18 @@
 def add(a,b):
     return a+b
 
+def mul(a,b):
+    return a*b
+
+
 def main(a,b):
     print("main function")
     
     add_result = add(a,b)
     print("add_result = ", add_result)
+
+    mul_result = mul(a,b)
+    print("mul_result = ", mul_result)
 
 if __name__ == '__main__':
     a=10

dbcd36b380d6794c1b551fc1d69af006d1894943
rahulgupta020 <rahul.gupta@factsonline.in>
2024-06-27 16:19:57 +0530

    adding code for addition function

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 
 
-def main():
+def add(a,b):
+    return a+b
+
+def main(a,b):
     print("main function")
+    
+    add_result = add(a,b)
+    print("add_result = ", add_result)
 
 if __name__ == '__main__':
-    main()
+    a=10
+    b=5
+    main(a=a, b=b)

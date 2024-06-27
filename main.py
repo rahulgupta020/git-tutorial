9e898562c0e67e9ac1dc59cde5930ac968efbdfc
rahulgupta020 <rahul.gupta@factsonline.in>
2024-06-27 16:38:06 +0530

    adding code for substraction function

--- main.py
+++ main.py
@@ -3,11 +3,18 @@
 def add(a,b):
     return a+b
 
+def sub(a,b):
+    return a-b
+
 def main(a,b):
     print("main function")
     
     add_result = add(a,b)
     print("add_result = ", add_result)
+
+    sub_result = sub(a,b)
+    print("sub_result = ", sub_result)
+
 
 if __name__ == '__main__':
     a=10
